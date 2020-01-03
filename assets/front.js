162491daf709ca47d792bd51a4f7d0311fbf0a4c
Kevin <kevin@rocher-dev.com>
2020-01-03 11:13:16 +0100

    v 0.2.1
    * Better behavior when using navigation in browser history.
    * Force loader display after 3 sec.
    * Performance fixes.

--- assets/front.js
+++ assets/front.js
@@ -9,19 +9,27 @@
         if (/in/.test(document.readyState) || !document.body) {
             setTimeout(function() {
                 domReady(func);
-            }, 9);
+            }, 18);
         }
         else {
             func();
         }
     }
+    domReady(set_loader);
 
-    domReady(function() {
+    function set_loader() {
         var $loader = document.querySelector('.page-loader');
+        if ($loader.classList.contains('is-disabled')) {
+            return;
+        }
         $loader.classList.add('is-disabled');
         window.addEventListener("beforeunload", function() {
             $loader.classList.remove('is-disabled');
         });
-    });
+    }
+
+    window.addEventListener("popstate", set_loader);
+
+    setTimeout(set_loader, 3000);
 
 }());
--- wpupagetransitions.php
+++ wpupagetransitions.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU Page Transitions
 Plugin URI: https://github.com/WordPressUtilities/wpupagetransitions
 Description: Add smooth transitions between pages without AJAX
-Version: 0.2.0
+Version: 0.2.1
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -12,7 +12,7 @@ License URI: http://opensource.org/licenses/MIT
 */
 
 class WPUPageTransitions {
-    private $plugin_version = '0.2.0';
+    private $plugin_version = '0.2.1';
     private $color = '#FFFFFF';
     private $duration = '1s';
 
